30a50df3c3ece007f39ad5eb0c657c0339064156
Charles Letellier <77594221+realcharlesletellier@users.noreply.github.com>
2021-02-24 21:41:18 -0500

    Update Sudoku-Solver.py

--- Sudoku-Solver.py
+++ Sudoku-Solver.py
@@ -10,50 +10,6 @@ board = [
     [0, 9, 0, 0, 0, 0, 0, 5, 0]
 ]
 
-print("Solving...")
-
-def solve(bo):
-    find = find_empty(bo)
-    if not find:
-        return True
-    else:
-        row, col = find
-
-    for i in range(1, 10):
-        if valid(bo, i, (row, col)):
-            bo[row][col] = i
-
-            if solve(bo):
-                return True
-
-            bo[row][col] = 0
-
-    return False
-
-
-def valid(bo, num, pos):
-    # Checks the row
-    for i in range(len(bo[0])):
-        if bo[pos[0]][i] == num and pos[1] != i:
-            return False
-
-        # Checks the column
-    for i in range(len(bo)):
-        if bo[i][pos[1]] == num and pos[0] != i:
-            return False
-
-    # Checks the box
-    box_x = pos[1] // 3
-    box_y = pos[0] // 3
-
-    for i in range(box_y * 3, box_y * 3 + 3):
-        for j in range(box_x * 3, box_x * 3 + 3):
-            if bo[i][j] == num and (i, j) != pos:
-                return False
-
-    return True
-
-
 def print_board(bo):
     # Prints the Sudoku Board
 
@@ -80,9 +36,55 @@ def find_empty(bo):
                 return (i, j)
     return None
 
+def valid(bo, num, pos):
+    # Begins checking if any given position is valid or not.
+
+    # Checks the row
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i] == num and pos[1] != i:
+            return False
+
+        # Checks the column
+    for i in range(len(bo)):
+        if bo[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # Checks the 3x3 box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if bo[i][j] == num and (i, j) != pos:
+                return False
+
+    return True
+
+print("Solving...")
+
+# Begins solving the Sudoku Board
+
+def solve(bo):
+    find = find_empty(bo)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1, 10):
+        if valid(bo, i, (row, col)):
+            bo[row][col] = i
+
+            if solve(bo):
+                return True
+
+            bo[row][col] = 0
+
+    return False
+
 
 print_board(board)
-print("Solved!")
 solve(board)
+print("Solved!")
 print("________________________")
 print_board(board)
